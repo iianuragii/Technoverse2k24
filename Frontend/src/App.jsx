efd4fc0ba90a10e5fd0055e8ae6c07fe9e8af8cd
Rajarshi Saha <saharajarshi14@gmail.com>
2024-07-27 00:17:26 +0530

    Added smart contract with deployment script, updated frontend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,7 +4,10 @@ import Dashboard from './Components/Dashboard'
 import About from './Components/About'
 import Faq from './Components/Faq'
 import { Routes, Route } from 'react-router-dom'
-
+import Upload from "../../artifacts/contracts/Upload.sol/Upload.json"
+import FileUpload from './Components/FileUpload'
+import Modal from "./Components/Modal"
+import { ethers } from "ethers";
 
 const App = () => {
   return (
---
+++ Frontend/src/Components/FileUpload.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function FileUpload() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default FileUpload
